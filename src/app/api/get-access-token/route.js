b42df9ee6331e0fc29a41cbc60c71a222b3f7502
Igor Marcossi <igormarcossi@hey.com>
2023-06-25 00:16:30 -0300

    jnj

--- src/app/api/get-access-token/route.js
+++ src/app/api/get-access-token/route.js
@@ -13,20 +13,23 @@ export async function POST(req) {
       const payload = Object.keys(params)
         .map((key) => `${key}=${encodeURIComponent(params[key])}`)
         .join("&");
-      const { data } = await axios.post(
-        "https://www.reddit.com/api/v1/access_token",
-        payload,
-        {
-          headers: { "content-type": "application/x-www-form-urlencoded" },
-        }
-      );
+
+
+        const options = {
+         method: 'POST',
+         headers: { 'content-type': 'application/x-www-form-urlencoded' },
+         data: payload,
+         url: 'https://www.reddit.com/api/v1/access_token',
+       }
+
+      const { data } = await axios(options);
 
       return NextResponse.json(
         { token: data.access_token, refresh: data.refresh_token },
         { status: 200 }
       );
     } catch {
-      return NextResponse.json({ error: "oops2" }, { status: 400 });
+      return NextResponse.json({ error: "oops2" }, { status: 403 });
     }
   } else {
     return NextResponse.json({ error: "oops" }, { status: 400 });
