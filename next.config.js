09efa595078a410caf2ebf2ce8ef571816af611b
Igor Marcossi <igormarcossi@hey.com>
2023-08-04 22:18:13 -0300

    Nice

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+   async rewrites() {
+      return {
+        afterFiles: [
+          // These rewrites are checked after pages/public files
+          // are checked but before dynamic routes
+          {
+            source: '/:path*',
+            destination: `/`,
+          },
+        ],
+      }
+    },
+}
 
 module.exports = nextConfig
