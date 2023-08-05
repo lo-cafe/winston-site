f97b218c407b4c0e8cb09bfa8befa363d9b452c7
Igor Marcossi <igormarcossi@hey.com>
2023-08-04 22:27:19 -0300

    Nicea

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,22 @@
 "use client";
 
+import {useEffect, useState} from "react";
 import Image from "next/navigation";
-import { usePathname } from "next/navigation";
 
 import styles from "./page.module.css";
 
 export default function Home() {
-  const pathname = usePathname();
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    setUrl(window.location.pathname)
+  }, []);
   return (
     <main className={styles.main}>
       <div>
         <img alt="Winston Logo" src="/transparent-winston.png" width="128" height="128" />
         <h2>Winston got ya</h2>
-        <a className={styles.nice} href={`winstonapp:/${pathname}`}>
+        <a className={styles.nice} href={`winstonapp:/${url}`}>
           Jump to Winston
         </a>
       </div>
